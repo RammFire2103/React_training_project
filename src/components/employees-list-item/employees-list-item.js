2d033ea5827d05481df75461e0b7c42f3b426d61
RammFire <korchinskij.mo@Gmail.com>
2024-04-10 22:48:09 +0300

    added employee deletion

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -27,9 +27,8 @@ class EmployeesListItem extends Component {
     }
 
     render() {
-        const {name, salary} = this.props;
+        const {name, salary, onDelete} = this.props;
         const {increase, likeEmployee} = this.state;
-
         let classNames = "list-group-item d-flex justify-content-between";
         if (increase) {
             classNames += ' increase';
@@ -51,7 +50,8 @@ class EmployeesListItem extends Component {
                     </button>
 
                     <button type="button"
-                            className="btn-trash btn-sm ">
+                            className="btn-trash btn-sm "
+                            onClick={onDelete}>
                         <i className="fas fa-trash"></i>
                     </button>
                     <i className="fas fa-star"></i>
